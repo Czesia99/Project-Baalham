03291a0aba6201a35074eca07b8ff581c8e48d9a
Balthazard Grego <balthazard.grego@epitech.eu>
2021-01-31 10:25:50 +0100

    Enemy Prefab Created + Light Source and Light Manager added (Enemy AI yet to be done)

---
+++ Assets/Scripts/LightManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+    public float Value;
+    public float MinValue;
+    public float MaxValue;
+    public float rate; 
+
+    private Light lightComponent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightComponent = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (Value < MaxValue)
+                Value += rate * Time.deltaTime; // Cap at some max value too
+        }
+        else if (Value > MinValue)
+        {
+            Value -= (rate / 5) * Time.deltaTime; // Cap at some min value too
+        }
+        lightComponent.intensity = Value;
+    }
+}
