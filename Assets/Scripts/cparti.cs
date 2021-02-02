fb7927b7b05542e128c3aecbc1a2206ab286d18e
yann <yann.berger@epitech.eu>
2021-02-02 22:54:29 +0100

    intro & outro

---
+++ Assets/Scripts/cparti.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cparti : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        player.transform.Rotate(new Vector3(0f, 180f, 0f));
+    }
+}
